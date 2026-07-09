7ef367084d609f66cd6bba65912f99e9db2c851a
Ezana Zecarias <ezac101@gmail.com>
2026-07-09 13:10:55 +0100

    live config url

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,8 +5,8 @@
  */
 
 // API base URL (no trailing slash)
-// window.API_BASE = "https://dev-hairlux-api-production.up.railway.app";
-window.API_BASE = "http://localhost:3000"; // localgoo
+window.API_BASE = "https://dev-hairlux-api-production.up.railway.app";
+// window.API_BASE = "http://localhost:3000"; // localgoo
 
 (function setupBackdropCleanup() {
 	function removeAll(selector) {
